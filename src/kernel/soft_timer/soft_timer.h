c70cd19829a243ef42efd48926bab8ab543b246b
llcc01 <2417224420@qq.com>
2023-01-06 23:00:09 +0800

    fix soft timer

--- src/kernel/soft_timer/soft_timer.h
+++ src/kernel/soft_timer/soft_timer.h
@@ -3,11 +3,23 @@
 
 #include "inc/types.h"
 
-struct soft_timer {
-    void (*func)(void *arg);
+struct soft_timer
+{
+    void (*callback)(void *arg);
     void *arg;
+    uint64_t interval;
     uint64_t next_tick;
+    uint16_t id;
 };
 
+#define SOFT_TIMER_ID_INVALID 0xFFFF
+
+void soft_timer_init();
+int soft_timer_cmp(void *a, void *b);
+uint16_t soft_timer_create(void (*callback)(void *arg), void *arg, uint32_t interval);
+void soft_timer_delete(struct soft_timer *timer);
+void soft_timer_calc_next();
+void soft_timer_heap_update();
+void soft_timer_handler();
 
 #endif /* __SOFT_TIMER_H__ */
